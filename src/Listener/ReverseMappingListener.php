5b24a35a244f064a0a10888f0ddb951acbf003aa
JeaNile <q962351668@gmail.com>
2023-01-17 17:38:10 +0800

    优化

--- src/Listener/ReverseMappingListener.php
+++ src/Listener/ReverseMappingListener.php
@@ -5,9 +5,6 @@ declare(strict_types=1);
 namespace Jeanile\SuffixQuery\Listener;
 
 use Hyperf\Contract\StdoutLoggerInterface;
-use Hyperf\Database\Model\Events\Event;
-use Hyperf\Database\Model\Events\Saved;
-use Hyperf\Database\Model\Model;
 use Hyperf\Event\Annotation\Listener;
 use Hyperf\Event\Contract\ListenerInterface;
 use Hyperf\Utils\Codec\Json;
@@ -42,15 +39,17 @@ class ReverseMappingListener implements ListenerInterface
      */
     public function process(object $event)
     {
-        if ($event instanceof Event) {
-            $model = $event->getModel();
+        if ($event instanceof ReverseMappingCreated || $event instanceof ReverseMappingUpdated) {
+            $model = $event->model;
+            $columns = sprintf('getReverseMapping%sColumns', $event instanceof ReverseMappingCreated ? 'Created' : 'Updated');
+
             $data = [];
-            // 业务事件继承后，自行传递需要映射的字段，此处只对映射数据做处理，不做业务逻辑
-            foreach ($model->getMappingColumns() as $mappingColumn) {
-                $data[] = $model->getAttribute($mappingColumn);
+            foreach ($model->{$columns}() as $mappingColumn) {
+                $model->getAttribute($mappingColumn) && $data[] = $model->getAttribute($mappingColumn);
             }
-            // TODO 考虑是否抽象不同驱动
+
             $this->logger->info(sprintf("reverse mapping:%s", Json::encode($data)));
+
             $data && $model->save($data);
         }
     }
